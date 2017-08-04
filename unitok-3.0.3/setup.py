b6a96fc3fdcd19d451c60a495f92191860040caa
Andrew Moore <a.moore@lancaster.ac.uk>
2017-08-04 05:34:19 +0100

    Correctly added unitok to requirements file and changed the pydot requirement.

--- unitok-3.0.3/setup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-from distutils.core import setup
-
-setup(name='unitok',
-      version='<version>',
-      description='Universal tokenizer',
-      packages=['unitok', 'unitok.configs'],
-)
